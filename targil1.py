35c98188956365ef0e48d90200e4b413df1dcbf6
bar shelly targil2 <barshelly731@gmail.com>
2020-11-10 11:49:48 +0200

    second lab.

---
+++ targil1.py
@@ -0,0 +1,8 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+mail=input("Enter your email: ")
+
+
+print("\nfull name: " + name +"bar"\nyour age is: " + str(age) + "\nyour mail: " + mail)
+
+
