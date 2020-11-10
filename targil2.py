fe3471ba62725f311213667c247704a99bc9295b
bar shelly targil2 <barshelly731@gmail.com>
2020-11-10 11:44:23 +0200

    second lab.

---
+++ targil2.py
@@ -0,0 +1,18 @@
+num=4567
+'''
+alafim=4
+meot=5
+asarot=6
+ahadot=7
+'''
+#this is alafim
+print("alafim=" + str(num//1000))
+
+#this is meot
+print("meot=" + str((num%1000)//100))
+
+#this is asarot
+print("asarot=" + str((num%100)//10))
+
+#this is ahadot
+print("ahadot=" + str((num%10)//1))
