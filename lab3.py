5403d82bd56d048874ec62013bb3d41f2faf37b0
bar shelly targil2 <barshelly731@gmail.com>
2020-11-11 13:04:57 +0200

    second lab.

---
+++ lab3.py
@@ -0,0 +1,12 @@
+hobbies="a tennis player,books,investment"
+job="i'm a student"
+age="22"
+name="bar shelly"
+
+#"bar ido chen tiki asaf aviv yana amit neli"
+print("\nmy hobbies is: "+(hobbies))
+print("\nwhat is yur job: " +(job))
+print("\nmy age is: " +  str(22*3))
+print("\nfull name: " + (name))
+print("\ninverted name:" + (name[::-1]))
+print("bar" in "bar ido chen tiki asaf aviv yana amit neli")
