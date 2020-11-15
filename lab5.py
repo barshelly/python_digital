24c7b7d03895ad31c2052b52194fb392fd8c65c5
bar shelly targil2 <barshelly731@gmail.com>
2020-11-15 11:25:15 +0200

    second lab.

--- lab5.py
+++ lab5.py
@@ -1,9 +1,10 @@
-names={"aviv":"100000","bar":"4567","natan":"990","asaf":"234"}
+names={"aviv":100,"bar":99,"asaf":23,"natan":56}
 print(names)
-summary=names["aviv"] + names["asaf"]
-print("the summary is: " + str(summary))
-
-names.update({"dudi":summary})
+names ["avi"]=250
 print (names)
+summary= ("the summary is: " + str(names["aviv"]+names ["avi"]))
+print (summary)
+names["shimi"]=350
+print(names)
 print("number of entries: " + str(len(names)))
 print("bar" in names)
