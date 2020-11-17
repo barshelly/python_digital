779f72cc1cf66ba7136656db197e2e4e956c6d6e
bar shelly targil2 <barshelly731@gmail.com>
2020-11-17 15:24:38 +0200

    second lab.

---
+++ targil6.py
@@ -0,0 +1,22 @@
+
+counter=10
+while(counter>0):
+    print(counter*100)
+    print("bar shelly")
+    counter=counter-1
+
+
+
+
+while(True):
+      name=input("enter a name: ")
+      if(name=="bar"):
+          print("hello bar")
+          break
+      elif(name=="bar"):
+          continue
+      else:
+        print("where is bar?")
+      number=int(input("enter a name: "))
+      print(number*4)
+      print("bye bye..")
